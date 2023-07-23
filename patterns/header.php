40a5617f4370ea7a736b0887eaa743e848940faa
Sarankumar994 <114742902+Sarankumar994@users.noreply.github.com>
2023-07-23 23:33:25 +0300

    initial commit and header section adjusted

---
+++ patterns/header.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Title: header
+ * Slug: solveurself/header
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:cover {"dimRatio":30,"overlayColor":"pale-cyan-blue","minHeight":580,"minHeightUnit":"px","contentPosition":"top center","isDark":false} -->
+<div class="wp-block-cover is-light has-custom-content-position is-position-top-center" style="min-height:580px"><span aria-hidden="true" class="wp-block-cover__background has-pale-cyan-blue-background-color has-background-dim-30 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group"><!-- wp:site-title /-->
+
+<!-- wp:separator -->
+<hr class="wp-block-separator has-alpha-channel-opacity"/>
+<!-- /wp:separator -->
+
+<!-- wp:navigation {"ref":15,"layout":{"type":"flex","justifyContent":"center"}} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:cover {"dimRatio":20,"overlayColor":"white","isDark":false} -->
+<div class="wp-block-cover is-light"><span aria-hidden="true" class="wp-block-cover__background has-white-background-color has-background-dim-20 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:paragraph -->
+<p>My name is Saran Kumar Reddy Kotimreddy. I love programming. As a hobby i developed some applications in various programming languages and i would like to share the experience and tips through this platform. </p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Follow page on</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:social-links -->
+<ul class="wp-block-social-links"><!-- wp:social-link {"url":"https://www.facebook.com/profile.php?id=100094658297749","service":"facebook","label":"Facebook Page Link"} /-->
+
+<!-- wp:social-link {"url":"https://twitter.com/solveurselfblog","service":"twitter","label":"Twitter Profile Link"} /-->
+
+<!-- wp:social-link {"url":"https://www.youtube.com/@solveurself","service":"youtube","label":"Youtube Channel Link"} /--></ul>
+<!-- /wp:social-links -->
+
+<!-- wp:paragraph -->
+<p>Follow me on</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:social-links -->
+<ul class="wp-block-social-links"><!-- wp:social-link {"url":"https://twitter.com/@sarankotimreddy","service":"twitter"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:group -->
+
+<!-- wp:image {"width":854,"height":853,"sizeSlug":"large","className":"is-style-rounded"} -->
+<figure class="wp-block-image size-large is-resized is-style-rounded"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/89602083_1326282107556616_8414934972703440896_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=mtChQX5fkWUAX_sJfU5&_nc_ht=scontent.fkwi5-2.fna&oh=00_AfAU4Vt8XLILDYDnDPJFVxCWhRh3DmWfBQz1UCcY4-7GVg&oe=64E4F4C7" alt="<?php echo esc_attr_e( 'Saran Kumar Profile Image', 'Solve Ur Self' ); ?>" width="854" height="853" title="Saran Kumar Profile Image"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover --></div></div>
+<!-- /wp:cover -->
